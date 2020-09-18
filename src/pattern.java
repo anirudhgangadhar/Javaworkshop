f1ba771c326175fca344fa38b795bdb1b036bb6f
anirudhgangadhar <anirudh1520@gmail.com>
2020-09-18 22:04:07 +0530

    Pattern

---
+++ src/pattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 5; i >= 1; i--) {
+            for (j = i; j >0; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
